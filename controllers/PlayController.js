6fff0e269a38628e57e72b68c1fc6de255bd8ad2
Muc Muc <tranhuutri0909@gmail.com>
2021-07-26 10:21:55 +0700

    fix death vote, valid tod members

--- controllers/PlayController.js
+++ controllers/PlayController.js
@@ -3,8 +3,8 @@ const path = require('path')
 class PlayController {
     index(req, res) {
         var specialChars = /[^a-zA-Z0-9 ]/g;
-        if(req.body.name.match(specialChars) && req.body.room.match(specialChars) && req.body.name.length < 30 && req.body.room.length < 20 && req.body.name.length > 0 && req.body.room.length > 0)
-            res.render(path.join(__dirname, "/../views/gamepage.ejs"),{ name: req.body.name, room: req.body.room });
+        if(req.body.name.length <= 30 && req.body.room.length <= 20 && req.body.name.length > 0 && req.body.room.length > 0 && !specialChars.test(req.body.name) && !specialChars.test(req.body.room))//req.body.name.match(specialChars) && req.body.room.match(specialChars) && 
+           res.render(path.join(__dirname, "/../views/gamepage.ejs"),{ name: req.body.name, room: req.body.room });
         else
             res.render(path.join(__dirname, "/../views/error.ejs"),{ vAlert: "Input is not valid" });
     }
