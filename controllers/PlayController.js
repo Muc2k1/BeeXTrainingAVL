1d22b13471530aa33316c071d1c6bfea948d6ac4
Muc Muc <tranhuutri0909@gmail.com>
2021-07-26 08:42:18 +0700

    valid in server

--- controllers/PlayController.js
+++ controllers/PlayController.js
@@ -2,8 +2,11 @@ const path = require('path')
 
 class PlayController {
     index(req, res) {
-        res.render(path.join(__dirname, "/../views/gamepage.ejs"),
-        { name: req.body.name, room: req.body.room });
+        var specialChars = /[^a-zA-Z0-9 ]/g;
+        if(req.body.name.match(specialChars) && req.body.room.match(specialChars) && req.body.name.length < 30 && req.body.room.length < 20 && req.body.name.length > 0 && req.body.room.length > 0)
+            res.render(path.join(__dirname, "/../views/gamepage.ejs"),{ name: req.body.name, room: req.body.room });
+        else
+            res.render(path.join(__dirname, "/../views/error.ejs"),{ vAlert: "Input is not valid" });
     }
 }
 
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,7 +1,10 @@
 const siteRouter = require('./site.route.js');
 const playRouter = require('./play.route.js');
+const errorRouter = require('./error.route.js');
+
 function route(app) {
     app.use("/play", playRouter);
+    app.use("/error", errorRouter);
     app.use("/", siteRouter);
 }
 module.exports = route;
